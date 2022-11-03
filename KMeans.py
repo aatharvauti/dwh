f7f8d419fb6fbea8e78dff0dcfbb83a0c00f78e7
aatharvauti <bm.aatharvauti@protonmail.ch>
2022-11-03 13:58:31 +0000

    KMeans Python

---
+++ KMeans.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+print(data)
+
+inertias = []
+
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(data)
+    inertias.append(kmeans.inertia_)
+
+plt.plot(range(1,11), inertias, marker='o')
+plt.title('Elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia')
+plt.show()
+
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(data)
+
+plt.scatter(x, y, c=kmeans.labels_)
+plt.show()
